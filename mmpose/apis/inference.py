8d6b2a6627d339ed1ee5c4d1e311d00c0b164ccb
Monal Kumar <Monalkumar745@gmail.com>
2022-08-04 10:35:40 +0530

    changes in inference

--- mmpose/apis/inference.py
+++ mmpose/apis/inference.py
@@ -294,8 +294,7 @@ def _inference_single_pose_model(model,
             print(icounter)
             input=batch_data['img'][icounter]
             input0 = tritonhttpclient.InferInput(input_name[0], (3, 256, 192), 'FLOAT32')
-            input0 = np.expand_dims(input0, axis=0)
-            #input0.set_data_from_numpy(input0)
+            input0.set_data_from_numpy(input0, binary_data=False)
             print(input0)
             output0 = tritonhttpclient.InferRequestedOutput(output_name,  binary_data=False)
             print(output0)
